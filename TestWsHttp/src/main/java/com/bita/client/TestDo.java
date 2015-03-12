84c361272cdd780f8f0a95d1b97f8d68dc777496
Mamadou <madouat@gmail.com>
2015-03-11 22:37:58 -0400

    added nouvelles choses

--- TestWsHttp/src/main/java/com/bita/client/TestDo.java
+++ TestWsHttp/src/main/java/com/bita/client/TestDo.java
@@ -5,6 +5,7 @@ public class TestDo {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("coucouc");
+        System.out.println("coucouc - madouat at");
 
     }
 
